adfcf1803719ad2b2181db886765ca21cba24eea
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-05-12 17:15:55 +0300

    added SplashScreen, WelcomeScreen and SignInScreen

--- app/src/main/java/com/example/webantpractice/MainActivity.kt
+++ app/src/main/java/com/example/webantpractice/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.sign_in_screen)
     }
 }
