035a83ec69477101ee6252dd0d1eb047fbbbb9a9
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-05-14 21:29:04 +0300

    fixed field input type. added add photo data screen

--- app/src/main/java/com/example/webantpractice/MainActivity.kt
+++ app/src/main/java/com/example/webantpractice/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.fragment_home)
+        setContentView(R.layout.fragment_add_photo)
     }
 }
