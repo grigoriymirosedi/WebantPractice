189d11d9af09121f04468866ebfb5f257905f6e7
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-05-14 03:59:53 +0300

    add Home screen

--- app/src/main/java/com/example/webantpractice/MainActivity.kt
+++ app/src/main/java/com/example/webantpractice/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_home)
     }
 }
