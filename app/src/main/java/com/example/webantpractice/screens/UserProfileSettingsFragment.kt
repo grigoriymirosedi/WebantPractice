e1c2100b8f422710066e0665af00d5c2f2edec74
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-05-16 12:31:15 +0300

    add back transition from UserProfukeSettings

--- app/src/main/java/com/example/webantpractice/screens/UserProfileSettingsFragment.kt
+++ app/src/main/java/com/example/webantpractice/screens/UserProfileSettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.webantpractice.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.webantpractice.R
 import com.example.webantpractice.databinding.FragmentUserProfileSettingsBinding
 
@@ -19,6 +21,13 @@ class UserProfileSettingsFragment : Fragment() {
     ): View? {
         binding = FragmentUserProfileSettingsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.userProfileSettingsToolbar.setNavigationOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_userProfileSettingsFragment_to_userProfileFragment)
+        }
     }
 
 }
