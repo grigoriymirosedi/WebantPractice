152ccdb2ba6e29df321319c55e113bef2803a36e
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-05-20 15:11:50 +0300

    Added PhotoGallery fragment
    added transition: FragmentPhotoGallery -> FragmentSetPhotoData;
    FragmentSetPhotoData -> FragmentPhotoGallery;
    
    Handled when user selects/closes photo picker

---
+++ app/src/main/java/com/example/webantpractice/screens/fragments/PhotoGalleryFragment.kt
@@ -0,0 +1,44 @@
+package com.example.webantpractice.screens.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.Navigation
+import com.example.webantpractice.R
+import com.example.webantpractice.databinding.FragmentPhotoGalleryBinding
+
+
+class PhotoGalleryFragment : Fragment() {
+
+    private lateinit var binding: FragmentPhotoGalleryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPhotoGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pickMultipleMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                // Callback is invoked after the user selects media items or closes the
+                // photo picker.
+                if (uri != null) {
+                    Navigation.findNavController(binding.root).navigate(R.id.action_photoGalleryFragment_to_setPhotoDataFragment)
+                } else {
+                    binding.errorImageSelectedIv.visibility = View.VISIBLE
+                    binding.errorImageTv.visibility = View.VISIBLE
+                }
+            }
+        pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+}
--- app/src/main/java/com/example/webantpractice/screens/fragments/SetPhotoDataFragment.kt
+++ app/src/main/java/com/example/webantpractice/screens/fragments/SetPhotoDataFragment.kt
@@ -1,11 +1,17 @@
 package com.example.webantpractice.screens.fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.Navigation
 import com.example.webantpractice.R
+import com.example.webantpractice.databinding.FragmentPhotoGalleryBinding
 import com.example.webantpractice.databinding.FragmentSetPhotoDataBinding
 
 
@@ -21,4 +27,10 @@ class SetPhotoDataFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.setPhotoDataToolbar.setNavigationOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_setPhotoDataFragment_to_photoGalleryFragment)
+        }
+    }
 }
